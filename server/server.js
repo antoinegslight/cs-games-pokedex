80c9ffccc9ef8299f685d42a6fcadca71035ab3a
Antoine GSlight <aslight21@outlook.com>
2016-11-01 13:52:49 -0400

    GET and POST method working

--- server/server.js
+++ server/server.js
@@ -8,6 +8,19 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 5678;
 var router = express.Router();
 
+var pokemons = [
+    {
+      "number": "004",
+      "name": "Charmander",
+      "type": "Fire"
+    },
+    {
+      "number": "025",
+      "name": "Pickachu",
+      "type": "Electric"
+    }
+];
+
 router.get('/', function(req, res) {
     res.json({ message: 'Pokedex' });
 });
@@ -16,18 +29,19 @@ router.route('/pokemons')
 
     .get(function(req, res) {
         res.json({
-            "pokemons": [
-                {
-                    "name": "Charmander",
-                    "type": "Fire"
-                },
-                {
-                  "name": "Pickachu",
-                  "type": "Electric"
-                }
-            ]
+            "pokemons": pokemons
         });
+    })
+
+    .post(function(req, res) {
+        var pokemon = {};
+        pokemon.number = req.body.number;
+        pokemon.name = req.body.name;
+        pokemon.type = req.body.type;
+        pokemons.push(pokemon);
+        res.json({ message: 'Pokemon created!' });
     });
+
 
 app.use('/pokedex', router);
 app.listen(port);
